b3f105de0558d90be8ac73e42e79e256e73ad382
Shiyu Wang <wangshiy@umich.edu>
2016-04-24 20:02:40 -0700

    official nodeMongo skeleton

---
+++ nodeMongoSeed/config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl: 'mongodb://wangshiy:Wgo2013sld@ds013891.mlab.com:13891/heroku_qd0ch192',
+    localUrl: 'mongodb://localhost/meanstacktutorials'
+};
